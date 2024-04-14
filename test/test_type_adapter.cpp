c2bb2966d6a34f3c411857f41a07cc3791f4c8b8
hakuturu583 <ms.kataoka@gmail.com>
2024-04-15 01:30:24 +0900

    update test code
    Signed-off-by: hakuturu583 <ms.kataoka@gmail.com>

--- test/test_type_adapter.cpp
+++ test/test_type_adapter.cpp
@@ -58,14 +58,16 @@ private:
     rclcpp::init(0, nullptr);                                                             \
     rclcpp::NodeOptions options;                                                          \
     options.use_intra_process_comms(true);                                                \
-    const auto point_cloud = pcl::PointCloud<pcl::POINT_TYPE>();                          \
+    const auto pub_point_cloud = pcl::PointCloud<pcl::POINT_TYPE>();                      \
     auto sub_node = std::make_shared<SubNode<pcl::PointCloud<pcl::POINT_TYPE>>>(          \
       options, [&](const pcl::PointCloud<pcl::POINT_TYPE> & point_cloud) {                \
-        EXPECT_TRUE(&point_cloud == &point_cloud);                                        \
+        EXPECT_TRUE(&pub_point_cloud == &point_cloud);                                    \
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("pub : ", << &pub_point_cloud));            \
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("sub : ", << &point_cloud));                \
         point_cloud_recieved = true;                                                      \
       });                                                                                 \
     auto pub_node = std::make_shared<PubNode<pcl::PointCloud<pcl::POINT_TYPE>>>(options); \
-    pub_node->publish(point_cloud);                                                       \
+    pub_node->publish(pub_point_cloud);                                                   \
     rclcpp::executors::SingleThreadedExecutor exec;                                       \
     exec.add_node(sub_node);                                                              \
     exec.add_node(pub_node);                                                              \
