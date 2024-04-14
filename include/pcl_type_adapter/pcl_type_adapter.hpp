f0fb4837cad09df1b4a0577e96b7474b05c8667b
Masaya Kataoka <ms.kataoka@gmail.com>
2024-04-14 21:10:19 +0900

    enable zero-copy pcl point cloud
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ include/pcl_type_adapter/pcl_type_adapter.hpp
@@ -0,0 +1,43 @@
+// Copyright 2024 OUXT Polaris. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PCL_TYPE_ADAPTER__PCL_TYPE_ADAPTER_HPP_
+#define PCL_TYPE_ADAPTER__PCL_TYPE_ADAPTER_HPP_
+
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+template <typename PCL_POINTCLOUD_TYPE>
+struct rclcpp::TypeAdapter<PCL_POINTCLOUD_TYPE, sensor_msgs::msg::PointCloud2>
+{
+  using is_specialized = std::true_type;
+  using custom_type = PCL_POINTCLOUD_TYPE;
+  using ros_message_type = sensor_msgs::msg::PointCloud2;
+
+  static void convert_to_ros_message(const custom_type & source, ros_message_type & destination)
+  {
+    pcl::toROSMsg(source, destination);
+  }
+
+  static void convert_to_custom(const ros_message_type & source, custom_type & destination)
+  {
+    pcl::fromROSMsg(source, destination);
+  }
+};
+
+#undef DEFINE_TYPE_ADAPTER
+
+#endif  // PCL_TYPE_ADAPTER__PCL_TYPE_ADAPTER_HPP_
---
+++ test/test_type_adapter.cpp
@@ -0,0 +1,81 @@
+// Copyright 2024 OUXT Polaris. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <functional>
+#include <pcl_type_adapter/pcl_type_adapter.hpp>
+
+namespace pcl_type_adapter
+{
+template <typename PCL_POINTCLOUD_TYPE>
+class PubNode : public rclcpp::Node
+{
+public:
+  using AdaptedType = rclcpp::TypeAdapter<PCL_POINTCLOUD_TYPE, sensor_msgs::msg::PointCloud2>;
+  explicit PubNode(const rclcpp::NodeOptions & options) : Node("test", options)
+  {
+    publisher_ = create_publisher<AdaptedType>("pointcloud", 1);
+  }
+  void publish(const PCL_POINTCLOUD_TYPE & point_cloud) { publisher_->publish(point_cloud); }
+
+private:
+  std::shared_ptr<rclcpp::Publisher<AdaptedType>> publisher_;
+};
+
+template <typename PCL_POINTCLOUD_TYPE>
+class SubNode : public rclcpp::Node
+{
+public:
+  using AdaptedType = rclcpp::TypeAdapter<PCL_POINTCLOUD_TYPE, sensor_msgs::msg::PointCloud2>;
+  explicit SubNode(
+    const rclcpp::NodeOptions & options,
+    const std::function<void(const PCL_POINTCLOUD_TYPE &)> function)
+  : Node("test", options)
+  {
+    subscriber_ = create_subscription<AdaptedType>("pointcloud", 1, function);
+  }
+
+private:
+  std::shared_ptr<rclcpp::Subscription<AdaptedType>> subscriber_;
+};
+
+TEST(TypeAdaptaer, PointXYZ)
+{
+  bool point_cloud_recieved = false;
+  rclcpp::init(0, nullptr);
+  rclcpp::NodeOptions options;
+  options.use_intra_process_comms(true);
+  const auto point_cloud = pcl::PointCloud<pcl::PointXYZ>();
+  auto sub_node = std::make_shared<SubNode<pcl::PointCloud<pcl::PointXYZ>>>(
+    options, [&](const pcl::PointCloud<pcl::PointXYZ> & point_cloud) {
+      EXPECT_TRUE(&point_cloud == &point_cloud);
+      point_cloud_recieved = true;
+    });
+  auto pub_node = std::make_shared<PubNode<pcl::PointCloud<pcl::PointXYZ>>>(options);
+  pub_node->publish(point_cloud);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(sub_node);
+  exec.add_node(pub_node);
+  exec.spin_some();
+  rclcpp::shutdown();
+  EXPECT_TRUE(point_cloud_recieved);
+}
+}  // namespace pcl_type_adapter
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
